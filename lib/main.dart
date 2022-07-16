a994e8e333adad608eee46107e3bfae776dd8e01
Kweayon  Clark <Kweayon@gmail.com>
2022-07-16 15:25:58 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:quotesapp/ui/homepage.dart';
-import 'package:quotesapp/network/model/listData.dart';
-import 'package:quotesapp/widget/quoteCard.dart';
-
-// Need to add image property to quote class
-// need to assign each quote the proper image
 
 void main() {
   return runApp(
